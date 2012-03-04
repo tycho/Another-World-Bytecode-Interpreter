5543d6e4ee2e7fd1e6236e09513667a9269416db
Steven Noonan <steven@uplinklabs.net>
2012-03-03 18:58:29 -0800

    sfxplayer: avoid shadowing 'res' variable, mainly to avoid compiler complaint
    Signed-off-by: Steven Noonan <steven@uplinklabs.net>

--- sfxplayer.cpp
+++ sfxplayer.cpp
@@ -23,8 +23,8 @@
 #include "sys.h"
 
 
-SfxPlayer::SfxPlayer(Mixer *mix, Resource *res, System *stub)
-	: mixer(mix), res(res), sys(stub), _delay(0), _resNum(0) {
+SfxPlayer::SfxPlayer(Mixer *mix, Resource *_res, System *stub)
+	: mixer(mix), res(_res), sys(stub), _delay(0), _resNum(0) {
 }
 
 void SfxPlayer::init() {
